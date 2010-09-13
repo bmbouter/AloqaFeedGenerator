f9f4801b8488f06a2a804f04cfbaee2545defe25
Brian Bouterse <bmbouter@gmail.com>
2010-09-13 17:10:33 -0400

    Added more to the Placemark model and added Placemark to the admin interface

---
+++ admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from models import Placemark
+
+admin.site.register(Placemark)
--- models.py
+++ models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 class Placemark(models.Model):
     name = models.CharField(max_length=128)
-    description = models.CharField(max_length=512)
+    description = models.TextField(max_length=512)
     latitude = models.FloatField()
     longitude = models.FloatField()
+    url = URLField(blank=True, verify_exists=True)
+    phone_number = models.IntegerField(blank=True)
+    email = models.EmailField(blank=True)
+    show_map = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return "%s Placemark" % self.name
